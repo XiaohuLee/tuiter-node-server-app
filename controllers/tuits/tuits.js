ad3f3f3409b134008cf63be9ada2dc9b137c07ca
Xiaohu Li <li.xiaohu3@northeastern.edu>
2022-11-19 15:22:30 -0500

    tuits.js changed

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -7,16 +7,16 @@ export default [
             " a spacefaring civilization is all about. It’s about believing in the future and thinking that the" +
             " future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
 "liked": true, "likes": 2345,
-    "replies": 123, "retuits": 432, "dislikes": 1234, "disliked": true
+    "replies": 123, "retuits": 432, "dislikes": 123, "disliked": true,
 },
 {
     "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-    "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+    "handle": "@boringcompany", "time": "2h", "image": "spacex.png",
     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being" +
         " building Mars Base 1",
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded" +
         " by Elon Musk. Its ongoing and proposed projects are designed for intracity ('loop') transit systems.",
     "liked": true, "likes": 2345,
-    "replies": 123, "retuits": 432, "dislikes": 1234, "disliked": true
+    "replies": 123, "retuits": 432, "dislikes": 123, "disliked": true,
 },
 ];
