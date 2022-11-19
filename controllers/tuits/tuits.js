68b04e8f39ad66796a7cf18595e0f0b84a9f8652
Xiaohu Li <li.xiaohu3@northeastern.edu>
2022-11-19 15:24:31 -0500

    tuits.js changed

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,7 +1,7 @@
 export default [
     {
         "_id": 123, "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
+        "handle": "@spacex", "time": "2h", "image": "spacex.jpg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - andthat’s what being" +
             " a spacefaring civilization is all about. It’s about believing in the future and thinking that the" +
@@ -11,7 +11,7 @@ export default [
 },
 {
     "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-    "handle": "@boringcompany", "time": "2h", "image": "spacex.png",
+    "handle": "@boringcompany", "time": "2h", "image": "spacex.jpg",
     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being" +
         " building Mars Base 1",
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded" +
