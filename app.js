afd71d970422df2d534f237d5df5e76852ed45b6
Xiaohu Li <li.xiaohu3@northeastern.edu>
2022-11-30 23:50:08 -0500

    a9

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://stalwart-sable-9480da.netlify.app/');
-    next();
-});
-app.use(cors())
+const corsOptions ={
+    origin:'*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 TuitsController(app);
 HelloController(app)
