59d6ef1d88c25e106dcd0a7b10f9c60e23ca7af6
Xiaohu Li <li.xiaohu3@northeastern.edu>
2022-11-30 23:32:58 -0500

    a9

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 const app = express()
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
